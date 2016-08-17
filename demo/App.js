675abe1ca2ac6ad5429817f4e18ecc41c7798473
Werner Punz <werpu@apache.org>
2016-08-17 19:20:34 +0200

    adding various world regions to the demo

--- demo/App.js
+++ demo/App.js
@@ -3,6 +3,6 @@ angular.module('App', ['bootstrap.picker'])
         $scope.greeting = "Hello World";
         $scope.currentDate = moment.tz(new Date(), "Europe/Zurich").startOf("day").toDate();
 
-        $scope.minDate = new Date();
-        $scope.maxDate = moment.tz(new Date(), "Europe/Zurich").add("month", 3).toDate();
+        $scope.minDate = moment.tz(new Date(), "Europe/Zurich").startOf("day").toDate();
+        $scope.maxDate = moment.tz(new Date(), "Europe/Zurich").endOf("day").add("month", 3).toDate();
     });
