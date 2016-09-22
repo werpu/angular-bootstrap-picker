7fc1f3d4fc67b1585f284daa37177aaf208dd572
Werner Punz <werner.punz@gmail.com>
2016-09-22 22:02:12 +0200

    display always 6 weeks to avoid jumpy visual months

--- dist/DatePickerFinal-amd.js
+++ dist/DatePickerFinal-amd.js
@@ -323,7 +323,7 @@ define("ViewModelBuilder", ["require", "exports", "DatePickerTypes"], function (
             }
             var momentDate = moment.tz(newValue, timezone);
             var start = moment.tz(newValue, timezone).startOf("month").startOf("week");
-            var end = moment.tz(newValue, timezone).endOf("month").endOf("week");
+            var end = moment.tz(newValue, timezone).startOf("month").startOf("week").add("days", 41);
             var momentStartDate = (startDate) ? startDate : null;
             var momentEndDate = (endDate) ? endDate : null;
             var range1 = moment.range(start, end);
